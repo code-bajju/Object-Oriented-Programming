10754263f542ef08a21dec89f69a827a778cb752
code-bajju <11212766@mmec.mmdu.org>
2023-09-12 21:09:16 +0530

    access modifier

--- src/AccessModifer.java
+++ src/AccessModifer.java
@@ -1,2 +1,12 @@
 public class AccessModifer {
+    public static void main(String[] args) {
+        student detail = new student();
+        detail.rollNo=11212766;
+        System.out.println(detail.rollNo);
+
+    }
+}
+class student{
+    int rollNo;
+    String name ;
 }
