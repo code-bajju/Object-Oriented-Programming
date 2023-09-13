5956ca36853809dcd489216ca376c776672d0c66
code-bajju <11212766@mmec.mmdu.org>
2023-09-13 23:09:39 +0530

    Indent

--- src/Basic.java
+++ src/Basic.java
@@ -1,2 +1,20 @@
 public class Basic {
+
+    static String Employee_name;
+    static float Employee_salary;
+
+    static void set(String n, float p) {
+        Employee_name = n;
+        Employee_salary = p;
+    }
+
+    static void get() {
+        System.out.println("Student name is: " + Employee_name);
+        System.out.println("Student CTC is: " + Employee_salary);
+    }
+
+    public static void main(String args[]) {
+        AlisaAndBajrang.set("Alisa and Bajrang", 10000.0f);
+        AlisaAndBajrang.get();
+    }
 }
