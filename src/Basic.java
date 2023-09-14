9a0984143da795aac9de26aed8fc2a706e39c4fc
code-bajju <11212766@mmec.mmdu.org>
2023-09-14 23:11:41 +0530

    Indent

--- src/Basic.java
+++ src/Basic.java
@@ -1,16 +1,16 @@
 public class Basic {
 
-    static String Employee_name;
-    static float Employee_salary;
+    static String Student_name;
+    static float Student_Sallery;
 
     static void set(String n, float p) {
-        Employee_name = n;
-        Employee_salary = p;
+        Student_name = n;
+        Student_Sallery = p;
     }
 
     static void get() {
-        System.out.println("Student name is: " + Employee_name);
-        System.out.println("Student CTC is: " + Employee_salary);
+        System.out.println("Student name is: " + Student_name);
+        System.out.println("Student CTC is: " + Student_Sallery);
     }
 
     public static void main(String args[]) {
